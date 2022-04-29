dfa3941def082a8f51d4087ab83cc76b62209ed1
Karen Guo <karengu0@live.unc.edu>
2022-04-29 17:56:26 -0400

    flip one coin

--- public/main.js
+++ public/main.js
@@ -1,8 +1,24 @@
+const { response } = require("express")
+
 // Focus div based on nav button click
 
 // Flip one coin and show coin image to match result when button clicked
-
+const coin = document.getElementById("coin")
+async function flipCoin() {
+    const endpoint = "app/flip/"
+    const url = document.baseURI+endpoint
+    await fetch(url)
+        .then(function(response) {
+        return response.json();
+    })
+    .then(function(result) {
+        console.log(result);
+        document.getElementById("result").innerHTML = result.flip;
+        document.getElementById("quarter").setAttribute("src", "assets/img/"+result.flip+".png");
+    });
+};
+coin.addEventListener("click", flipCoin)
 // Flip multiple coins and show coin images in table as well as summary results
-// Enter number and press button to activate coin flip series
+
 
 // Guess a flip by clicking either heads or tails button
